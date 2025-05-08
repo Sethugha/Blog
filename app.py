bae25ee94560937643b57629f0c7cf8bd6ab3418
Sethugha <gerd.furmanek@gmx.net>
2025-05-08 07:34:50 +0200

    pages still crude but adding posts works

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import storage
 app = Flask(__name__)
 
@@ -7,6 +7,22 @@ app = Flask(__name__)
 def index():
     data = storage.load_json('data.json')
     return render_template('index.html', data=data)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        author = request.form['author']
+        title = request.form['title']
+        content = request.form.get('content','Lorem Ipsum')
+        id = storage.get_highest_id() + 1
+        data = storage.load_json('data.json')
+        data.append({'id': id, 'author': author, 'title': title, 'content': content})
+        storage.save_json(data)
+        return redirect(url_for('index'))
+    return render_template('add.html')
+
+
+
 
 
 
--- storage.py
+++ storage.py
@@ -1,13 +1,30 @@
 import json
 
+
 def save_json(data):
     with open('data.json', 'w', encoding='utf-8') as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
+
 
 def load_json(filepath):
     with open(filepath, 'r') as file:
         data = json.load(file)
     return data
+
+
+def get_highest_id():
+    ids = []
+    data = load_json('data.json')
+    for post in data:
+        ids.append(post['id'])
+    return max(ids)
+
+
+def delete_post(id):
+    data = load_json('data.json')
+    for post in data:
+        if post['id'] == id:
+            data.pop(post.index())
 
 
 def main():
@@ -16,6 +33,7 @@ def main():
         {'id': 2, 'author': 'Jane Doe', 'title': 'Second Post', 'content': 'This is another post.'},
         ]
     save_json(data)
+    get_highest_id()
 
 if __name__ == '__main__':
     main()
